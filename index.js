238696577ab02261f7c0c1b5610a879b435a7a88
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-25 11:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,5 +64,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber == "") {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+
+  cart = [];
+  var total = total();
+  return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`;
 }
