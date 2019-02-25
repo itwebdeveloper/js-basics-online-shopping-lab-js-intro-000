41676e6c836fdeae682f496ca9d2e8c373c5c3f3
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-25 11:29:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,6 +60,8 @@ function removeFromCart(item) {
         } else {
           return "That item is not in your cart.";
         }
+      } else {
+          return "That item is not in your cart.";
       }
     }
     return cart;
