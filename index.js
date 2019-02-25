fbda6f80f3e890cf5fa97b6f492fed7bd08e669f
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-25 11:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
     return "That item is not in your cart.";
   } else {
     for(let i = 0; i < cart.length; i++) {
-      if (cart != [] && cart[i] !== undefined) {
+      if (cart[i] !== undefined) {
         if (cart[i].itemName == item) {
           cart.splice(i, 1);
         } else {
